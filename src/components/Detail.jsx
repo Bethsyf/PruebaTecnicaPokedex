0dba09d1dc4f5dc09e797ada41a2349a71818dc5
Bethsyf <bethsyfalcon@gmail.com>
2022-05-24 15:25:09 -0500

    mostrar detalle de PokeApi

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,72 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Card, CardGroup, ListGroup } from 'react-bootstrap'
 
 const Detail = () => {
+  const [detail, setDetail] = useState([]);
 
+  useEffect(() => {
+    const detailPokemon = [JSON.parse(localStorage.getItem('detailPokemon'))];
+    setDetail(detailPokemon);
+  }, [])
   return (
     <>
       <div className="">
-        <div className="d-flex justify-content-center">
-          <Card className="col-sm-auto m-3" style={{ width: '14rem' }} >
-            <Card.Title className="text-warning" >#</Card.Title>
-            <Card.Img variant="top" src='imagen' />
-            <Card.Title className="text-warning" >nombre</Card.Title>
-          </Card>
-          <Card style={{ width: '18rem' }}>
-            <ListGroup variant="flush">
-              <ListGroup.Item>Altura: </ListGroup.Item>
-              <ListGroup.Item>Peso: </ListGroup.Item>
-              <ListGroup.Item>Habilidades:</ListGroup.Item>
-              <ListGroup.Item>Tipo: </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </div>
-        <div className="m-5">
-          <Card.Title className="text-warning" >Imagenes del Pokemon</Card.Title>
-          <CardGroup >
-            <Card>
-              <Card.Img variant="top" src='https://res.cloudinary.com/dmaviub4l/image/upload/v1653261157/urntbgstiaund9xgey0i.webp' />
-              <Card.Body>
-                <Card.Title>left</Card.Title>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Img variant="top" src='https://res.cloudinary.com/dmaviub4l/image/upload/v1653261157/urntbgstiaund9xgey0i.webp' />
-              <Card.Body>
-                <Card.Title>back</Card.Title>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Img variant="top" src='https://res.cloudinary.com/dmaviub4l/image/upload/v1653261157/urntbgstiaund9xgey0i.webp' />
-              <Card.Body>
-                <Card.Title>rigth</Card.Title>
-              </Card.Body>
-            </Card>
-          </CardGroup>
-        </div>
-        <div className="m-5">
-          <Card.Title className="text-warning" >Evoluciones</Card.Title>
-          <CardGroup>
-            <Card>
-              <Card.Img variant="top" src='https://res.cloudinary.com/dmaviub4l/image/upload/v1653261157/urntbgstiaund9xgey0i.webp' />
-              <Card.Body>
-                <Card.Title>Pikachu1</Card.Title>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Img variant="top" src='https://res.cloudinary.com/dmaviub4l/image/upload/v1653261157/urntbgstiaund9xgey0i.webp' />
-              <Card.Body>
-                <Card.Title>Pikachu2</Card.Title>
-              </Card.Body>
-            </Card>
-            <Card>
-              <Card.Img variant="top" src='https://res.cloudinary.com/dmaviub4l/image/upload/v1653261157/urntbgstiaund9xgey0i.webp' />
-              <Card.Body>
-                <Card.Title>Pikachu3</Card.Title>
-              </Card.Body>
-            </Card>
-          </CardGroup>
-        </div>
+        {
+          detail?.map((pokemon, index) => (
+            <div>
+              <div className="d-flex justify-content-center">
+                <Card className="col-sm-auto m-3" key={index} style={{ width: '18rem' }} >
+                  <Card.Title className="text-warning" >#{pokemon.id}</Card.Title>
+                  <Card.Img variant="top" src={pokemon.sprites.other.dream_world.front_default} />
+                  <Card.Title className="text-warning" >{pokemon.name}</Card.Title>
+                </Card>
+                <Card style={{ width: '18rem' }}>
+                  <ListGroup variant="flush">
+                    <ListGroup.Item>Tipos: {pokemon.types[0].type.name} / {pokemon.types[1] ? pokemon.types[1].type.name : null}</ListGroup.Item>
+                    <ListGroup.Item>Habilidades: {pokemon.abilities[0].ability.name} / {pokemon.abilities[1] ? pokemon.abilities[1].ability.name : null}</ListGroup.Item>
+                    <ListGroup.Item>Principales movimientos: {pokemon.moves[0].move.name} - {pokemon.moves[1] ? pokemon.moves[1].move.name : null} - {pokemon.moves[2] ? pokemon.moves[2].move.name : null} - {pokemon.moves[3] ? pokemon.moves[3].move.name : null}</ListGroup.Item>
+                    <ListGroup.Item>Experiencia base: {pokemon.base_experience}</ListGroup.Item>
+                    <ListGroup.Item>Altura: {(pokemon.height) / 10} m.</ListGroup.Item>
+                    <ListGroup.Item>Peso: {(pokemon.weight) / 10} kg.</ListGroup.Item>
+                  </ListGroup>
+                </Card>
+              </div>
+              <div className="m-5">
+                <Card.Title className="text-warning" >Imagenes del Pokemon</Card.Title>
+                <CardGroup >
+                  <Card>
+                    <Card.Img variant="top" src={pokemon.sprites.other.home.front_default} />
+                    <Card.Body>
+                      <Card.Title>Color 1</Card.Title>
+                    </Card.Body>
+                  </Card>
+                  <Card>
+                    <Card.Img variant="top" src={pokemon.sprites.other.home.front_shiny} />
+                    <Card.Body>
+                      <Card.Title>Color 2</Card.Title>
+                    </Card.Body>
+                  </Card>
+                </CardGroup>
+              </div>
+            </div>
+          ))
+        }
       </div>
     </>
   )
--- src/components/HomeApi.jsx
+++ src/components/HomeApi.jsx
@@ -8,53 +8,51 @@ const HomeApi = () => {
   const [pokeArray, setPokeArray] = useState([]);
   const getData = async (url) => {
     axios.get(url)
-        .then(resp => {
-            for (let i = 0; i < resp.data.results.length; i++) {
-                setPokeArray([])
-                axios.get(resp.data.results[i].url)
-                    .then(result => {
-                        setPokeArray(pokeArray => [...pokeArray, result.data])
-                    })
-            }
-        })
-}
-useEffect(() => {
-  getData(url);
-}, [url])
+      .then(resp => {
+        for (let i = 0; i < resp.data.results.length; i++) {
+          setPokeArray([])
+          axios.get(resp.data.results[i].url)
+            .then(result => {
+              setPokeArray(pokeArray => [...pokeArray, result.data])
+            })
+        }
+      })
+  }
 
+  const showDetail = (detailPokemon) => {
+    localStorage.setItem('detailPokemon', JSON.stringify(detailPokemon))
+    console.log(detailPokemon)
+  }
+
+  useEffect(() => {
+    getData(url);
+  }, [])
 
   return (
     <>
-    <div className="bg-transparent">
-      <div className="row m-3" >
-      {
-  pokeArray?.map((pokemon, index) => (  
-  
-            <Card className="col-sm-auto m-3 pokemon" key={index}  style={{ width: '14rem' }} >
-              <Card.Img variant="top" src={pokemon.sprites.other.dream_world.front_default} />
-              <Card.ImgOverlay className="">
-                <div className="d-flex justify-content-end">
-                  <Card.Title className="text-warning" >#{pokemon.id}</Card.Title>                                    
-                </div>                
-              </Card.ImgOverlay>
-              <div className="description">
-                <Card.Title className="text-danger" >{pokemon.name}</Card.Title>
-                <Card.Text className="text-danger mb-5" >Tipo: {pokemon.types[0].type.name} </Card.Text>
-                <div><Button className="card-button" href={`/detail/${pokemon.name}`}>Ver más... detalles</Button></div>
-              </div>
-            </Card>
-            
+      <div className="bg-transparent">
+        <div className="row m-3" >
+          {
+            pokeArray?.map((pokemon, index) => (
+              <Card className="col-sm-auto m-3 pokemon" key={index} style={{ width: '14rem' }} >
+                <Card.Img variant="top" src={pokemon.sprites.other.dream_world.front_default} />
+                <Card.ImgOverlay className="">
+                  <div className="d-flex justify-content-end">
+                    <Card.Title className="text-warning" >#{pokemon.id}</Card.Title>
+                  </div>
+                </Card.ImgOverlay>
+                <div className="description">
+                  <Card.Title className="text-danger" >{pokemon.name}</Card.Title>
+                  <Card.Text className="text-danger mb-5" >Tipos: {pokemon.types[0].type.name} / {pokemon.types[1] ? pokemon.types[1].type.name : null} </Card.Text>
+                  <div><Button className="card-button" href={`/detail/${pokemon.name}`} onClick={() => showDetail(pokemon)}>Ver más... detalles</Button></div>
+                </div>
+              </Card>
             ))
-          
-  }    
-        
+          }
+        </div>
       </div>
-      
-    </div>
     </>
-    
   )
-  
 }
 
 export default HomeApi
